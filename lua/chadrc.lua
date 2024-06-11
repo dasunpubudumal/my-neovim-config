e8e6a55e83e92c843b5297e1ca18e08d180234e9
Dasun Pubudumal <pubudumald@gmail.com>
2024-06-11 19:32:50 +0100

    Adding dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -11,6 +11,41 @@ M.ui = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
 	-- },
+
+	nvdash = {
+		load_on_startup = true,
+	
+		header = {
+		  "           ▄ ▄                   ",
+		  "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+		  "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+		  "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+		  "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+		  "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+		  "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+		  "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+		  "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+		},
+	
+		buttons = {
+		  { "  Find File", "Spc f f", "Telescope find_files" },
+		  { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+		  { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+		  { "  Bookmarks", "Spc m a", "Telescope marks" },
+		  { "  Themes", "Spc t h", "Telescope themes" },
+		  { "  Mappings", "Spc c h", "NvCheatsheet" },
+		},
+	  },
+
+	  statusline = {
+		theme = "minimal", -- default/vscode/vscode_colored/minimal
+		-- default/round/block/arrow separators work only for default statusline theme
+		-- round and block will work for minimal theme only
+		separator_style = "default",
+		order = nil,
+		modules = nil,
+	  },
+	
 }
 
 return M
